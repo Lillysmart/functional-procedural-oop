c956821b2b7330b25437cc1603f89be3a1f66cf4
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-06 10:49:10 +0200

    create a condition to check the userinput

--- procedural.js
+++ procedural.js
@@ -11,8 +11,20 @@ event.preventDefault()
 const enteredUserName =userName.value
 const enteredUserPassword= userPassword.value
 if (userName.trim().length ===0){
-
+alert('Invalid Input, Userame must not be empty ')
+return
 }
+if (userPassword.trim().length <=8){
+  alert('Invalid Input, Password must be 9 characters or longer  ')
+  return
+  }
+
+  const user ={
+    userNameInput:enteredUserName,
+    userPasswordInput :enteredUserPassword
+  }
+  console.log (user)
+  console.log (`hi , i am ${user.userNameInput}`)
 }
 
 userForm.addEventListener('submit',sugnUpHandler);
