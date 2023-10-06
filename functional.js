96b23d383aab81993683ab8798c3c63801b80062
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-10-06 12:30:23 +0200

    break down the functions

--- functional.js
+++ functional.js
@@ -1,9 +1,19 @@
-const signUpHandler = (event) => {
-  event.preventDefault();
+const validateUser=()=>{
   
+}
+
+const getUserInput = (inputElementdata) => {
+return document.querySelector(inputElementdata).value
 };
 
-const connectForm = (formid, formSubmitHandler) => {
+
+const signUpHandler = (event) => {
+  event.preventDefault();
+  const enteredUser =getUserInput("[data-user-username]")
+  const enteredPassword=getUserInput("[data-user-password]")
+};
+
+const connectForm = (formdata, formSubmitHandler) => {
   const userForm = document.querySelector(formid);
   userForm.addEventListener("submit", formSubmitHandler);
 };
